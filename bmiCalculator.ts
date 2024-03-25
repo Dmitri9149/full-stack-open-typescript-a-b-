dccda0c844f713e9ae07f74e08d23d7661558b66
Dmitri9149 <dmitri.as.name@gmail.com>
2024-03-25 19:34:23 +0200

    ex 9.1

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,8 +1,9 @@
 const calculateBmi = (height: number, weight: number): string  => {
-  const index: number = weight / (height + height);
-  if (index <= 18.5) { return `Underweight`} 
-  else if (index > 18.5 && index <= 24.9) {return `Normal (healthy weight)`} 
-  else { return `Overweight`};
+  const index: number = weight / (height * height * 0.0001);
+  if (index < 18.5) { return `Underweight`} 
+  else if (index >= 18.5 && index <= 24.9) {return `Normal (healthy weight)`} 
+  else if (index > 24.9 && index <= 29.9) { return `Overweight`} 
+  else { return `Obesity` };
 } 
 
 console.log(calculateBmi(180, 74))
