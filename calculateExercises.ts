865b2ea0f589b9d0ec18cd60795e2be937a676d8
Dmitri9149 <dmitri.as.name@gmail.com>
2024-04-21 20:10:10 +0300

    define function to parse arguments

--- calculateExercises.ts
+++ calculateExercises.ts
@@ -8,6 +8,27 @@ interface ExercisesInfo {
   average: number 
 }
 
+interface ArgsData {
+  records: number[]
+}
+
+const parseArguments1 = (args: string[]):ArgsData => {
+  if (args.length < 2) throw new Error('Not enough arguments');
+  if (args.length === 2) throw new Error('Training records were not provided');
+  const maybeData = args.slice(1);
+  let validArgs = true;
+    for (const arg in maybeData) {
+      validArgs = validArgs && !isNaN(Number(arg))
+    } 
+  if (validArgs) {
+    return {
+      records: maybeData.map( d => Number(d)) 
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+} 
+
 const calculateExercises = 
   (daily_hours: number[], target: number) : ExercisesInfo => {
     const sum = daily_hours.reduce((a,b) => a + b, 0);
