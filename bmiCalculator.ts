89921767505c6d0d9f8bce733b7149c05314e812
Dmitri9149 <dmitri.as.name@gmail.com>
2024-04-21 18:55:10 +0300

    change height from cm to height in meters

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,5 +1,6 @@
 const calculateBmi = (height: number, weight: number): string  => {
-  const index: number = weight / (height * height * 0.0001);
+  const height_m = height * 0.01;
+  const index: number = weight / (height_m * height_m);
   if (index < 18.5) { return `Underweight`} 
   else if (index >= 18.5 && index <= 24.9) {return `Normal (healthy weight)`} 
   else if (index > 24.9 && index <= 29.9) { return `Overweight`} 
