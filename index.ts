781b90983af49de2074f18dfc96c737e11ee58d1
Dmitri9149 <dmitri.as.name@gmail.com>
2024-04-28 19:01:38 +0300

    specify request to /bmi endpoint usong query string parameters, response result of bmiCalculate

--- index.ts
+++ index.ts
@@ -1,8 +1,38 @@
 import express from 'express';
+import calculateBmi from './bmiCalculator';
+// import calculateExercises from './calculateExercises';
 const app = express();
 
-app.get('/ping', (_req, res) => {
-  res.send('pong');
+app.use(express.json());
+
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack');
+});
+
+app.get('/bmi', (req, res) => {
+  const {height, weight} = req.query;
+
+  try {
+    const heightNumber: number = parseFloat(height as string);
+    const weightNumber: number = parseFloat(weight as string);
+
+    if (isNaN(heightNumber) || isNaN(weightNumber) ) {
+      throw Error();
+    }
+
+    const bmi = calculateBmi(heightNumber, weightNumber);
+
+    return res.json({
+      weight: weightNumber,
+      height: heightNumber,
+      bmi: bmi
+    });
+  }
+    catch(error) {
+      return res.status(404).json({
+        error: "malformed parameters"
+      })
+    }
 });
 
 const PORT = 3003;
